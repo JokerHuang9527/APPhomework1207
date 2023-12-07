56b2c08fcb85b74b69574a72c30694ee271ac45c
110360131joker <huangpw1031@gmail.com>
2023-12-07 11:27:25 +0800

    master

---
+++ src/hw5/sample.java
@@ -0,0 +1,19 @@
+package hw5;
+
+public class sample {
+
+	public static void main(String[] args) {
+		/*
+		//Sample 5-1
+		Car car1;
+		car1 = new Car();
+		car1.show();
+		
+		//Sample 5-2
+		Car car1;
+		car1 = new Car();
+		car1.show();
+		*/
+	}
+
+}
